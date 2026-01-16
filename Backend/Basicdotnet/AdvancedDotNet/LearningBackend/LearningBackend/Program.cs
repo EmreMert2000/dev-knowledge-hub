86963626e1c0f7a1c86e34601dbd2cf90caffb25
Emre Mert <61905534+EmreMert2000@users.noreply.github.com>
2026-01-16 17:49:00 +0300

    Transform File Backend Learning

---
+++ Backend/Basicdotnet/AdvancedDotNet/LearningBackend/LearningBackend/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningBackend
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection("Data Source = NIZAMYAPı;initial Catalog = devhub;integrated security = true");
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("Select * From TblCategory", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+
+            adapter.Fill(dataTable);
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.Read();
+
+        }
+    }
+}
